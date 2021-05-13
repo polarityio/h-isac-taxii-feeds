8b46fb0216bddba1916fa84d10abe1d1a0a88aa4
Jon Penwood <jonp@polarity.io>
2021-05-13 12:39:34 -0500

    Removed unused component functions

--- components/block.js
+++ components/block.js
@@ -1,10 +1,3 @@
 polarity.export = PolarityComponent.extend({
-  details: Ember.computed.alias('block.data.details'),
-  summary: Ember.computed.alias('block.data.summary'),
-  init() {
-    this._super(...arguments);
-  },
-  actions: {
-
-  }
+  details: Ember.computed.alias('block.data.details')
 });
